1752db7f2fde2ef5d813f836adade78a8b71890e
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-18 20:19:52 -0700

    started routes and react-navigation configuration

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <SafeAreaView>
+      <Text>Sign in</Text>
+    </SafeAreaView>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '~/pages/SignIn';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SignIn" component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
