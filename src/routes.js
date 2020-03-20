04def3181e2e55644c3cd9a31badeb709cf9b049
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-19 20:11:04 -0700

    started signin and signup routes

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,37 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+import Colors from '~/styles/colors';
 
 const Stack = createStackNavigator();
 
 function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Navigator
+        initialRouteName="SignIn"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: Colors.primary,
+          },
+          headerTintColor: Colors.secondary,
+          headerTitleStyle: {
+            fontWeight: '600',
+          },
+        }}
+      >
+        <Stack.Screen
+          options={{ header: () => null }}
+          name="SignIn"
+          component={SignIn}
+        />
+        <Stack.Screen
+          options={{ header: () => null }}
+          name="SignUp"
+          component={SignUp}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
