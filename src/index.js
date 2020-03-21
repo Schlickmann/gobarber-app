34877da737ae61adb92536f5206deeff32d214ec
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-20 21:23:58 -0700

    added splash screen module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar, Platform } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import Routes from '~/routes';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      {Platform.OS === 'ios' && <StatusBar barStyle="light-content" />}
       <Routes />
     </>
   );
