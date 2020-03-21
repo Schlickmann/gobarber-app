ffefd62224d5bf96edbfd66a5396252622009f0d
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-20 20:18:07 -0700

    added logo images

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Image } from 'react-native';
 
-import logo from '~/assets/logo.svg';
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 import Input from '~/components/Input';
 import Button from '~/components/Button';
@@ -10,6 +11,7 @@ import Button from '~/components/Button';
 export default function SignIn({ navigation }) {
   return (
     <Background>
+      <Image source={logo} />
       <Input
         style={{ marginTop: 30 }}
         icon="mail"
