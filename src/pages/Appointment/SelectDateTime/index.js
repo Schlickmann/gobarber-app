c9efc2a1f17958fa47a21952daa3754a0e057c13
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-04-01 18:51:25 -0700

    added elements to select date time page

--- src/pages/Appointment/SelectDateTime/index.js
+++ src/pages/Appointment/SelectDateTime/index.js
@@ -1,18 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
+import api from '~/services/api';
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
-import { Container } from './styles';
+import { Container, HourList, Hour, Title } from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ route, navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const { provider } = route.params;
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectTime(time) {
+    navigation.navigate('Confirmation', { provider, time });
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              onPress={() => handleSelectTime(item.value)}
+              enabled={item.available}
+            >
+              <Title enabled={item.available}>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
 }
+
+SelectDateTime.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      provider: PropTypes.shape({
+        id: PropTypes.number,
+      }),
+    }),
+  }).isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
