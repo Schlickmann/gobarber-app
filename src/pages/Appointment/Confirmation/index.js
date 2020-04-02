73ce847582b8f88e22942a7b794faec97c34603b
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-04-01 19:20:04 -0700

    added elements to confirmation page

--- src/pages/Appointment/Confirmation/index.js
+++ src/pages/Appointment/Confirmation/index.js
@@ -1,12 +1,77 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { formatRelative, parseISO } from 'date-fns';
 
+import api from '~/services/api';
+import Alert from '~/components/Alert';
 import Background from '~/components/Background';
-import { Container } from './styles';
+import Colors from '~/styles/colors';
 
-export default function Confirmation() {
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirmation({ route, navigation }) {
+  const { provider, time } = route.params;
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date()),
+    [time]
+  );
+
+  async function handleSubmit() {
+    try {
+      await api.post('/appointments', { provider_id: provider.id, date: time });
+
+      await Alert('Appointment Success', 'Appointment booked successfully');
+
+      navigation.navigate('Dashboard');
+    } catch (error) {
+      await Alert(
+        'Appointment Failure',
+        'Something went wrong, please try again later'
+      );
+    }
+  }
+
   return (
     <Background>
-      <Container />
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton
+          color={Colors.secondary}
+          // loading={loading}
+          onPress={handleSubmit}
+        >
+          Book Now
+        </SubmitButton>
+      </Container>
     </Background>
   );
 }
+
+Confirmation.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      provider: PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        avatar: PropTypes.shape({
+          url: PropTypes.string,
+        }),
+      }),
+      time: PropTypes.string,
+    }),
+  }).isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
--- src/pages/Appointment/Confirmation/styles.js
+++ src/pages/Appointment/Confirmation/styles.js
@@ -1,3 +1,42 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Button from '~/components/Button';
+import Colors from '~/styles/colors';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 30px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  height: 120px;
+  width: 120px;
+  border-radius: 60px;
+`;
+
+export const Name = styled.Text`
+  margin-top: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  color: ${Colors.light};
+  text-align: center;
+  text-transform: capitalize;
+`;
+
+export const Time = styled.Text`
+  margin-top: 4px;
+  font-size: 18px;
+  font-weight: bold;
+  color: ${Colors.light};
+  text-align: center;
+  opacity: 0.6;
+  /* text-transform: capitalize; */
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 20px;
+  align-self: stretch;
+`;
